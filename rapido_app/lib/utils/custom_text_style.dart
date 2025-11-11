d00af7ccc08bfbf129bed7982950f3159cbcdc87
Rishabh7778 <ss7524294@gmail.com>
2025-11-11 12:45:11 +0530

    text style done

---
+++ rapido_app/lib/utils/custom_text_style.dart
@@ -0,0 +1,88 @@
+
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+TextStyle myTextStyle15({
+  Color textColor=Colors.black,
+  String fontFamily= "mainFont",
+  FontWeight fontWeight= FontWeight.bold
+}){
+  return TextStyle(
+    color: textColor,
+    fontFamily: fontFamily,
+    fontSize: 15
+  );
+}
+
+TextStyle myTextStyle18({
+  Color textColor=Colors.black,
+  String fontFamily= "mainFont",
+  FontWeight fontWeight= FontWeight.bold
+}){
+  return TextStyle(
+      color: textColor,
+      fontFamily: fontFamily,
+      fontSize: 18
+  );
+}
+
+TextStyle myTextStyle20({
+  Color textColor=Colors.black,
+  String fontFamily= "mainFont",
+  FontWeight fontWeight= FontWeight.bold
+}){
+  return TextStyle(
+      color: textColor,
+      fontFamily: fontFamily,
+      fontSize: 20
+  );
+}
+
+TextStyle myTextStyle22({
+  Color textColor=Colors.black,
+  String fontFamily= "mainFont",
+  FontWeight fontWeight= FontWeight.bold
+}){
+  return TextStyle(
+      color: textColor,
+      fontFamily: fontFamily,
+      fontSize: 22
+  );
+}
+
+TextStyle myTextStyle25({
+  Color textColor=Colors.black,
+  String fontFamily= "mainFont",
+  FontWeight fontWeight= FontWeight.bold
+}){
+  return TextStyle(
+      color: textColor,
+      fontFamily: fontFamily,
+      fontSize: 25
+  );
+}
+
+TextStyle myTextStyle30({
+  Color textColor=Colors.black,
+  String fontFamily= "mainFont",
+  FontWeight fontWeight= FontWeight.bold
+}){
+  return TextStyle(
+      color: textColor,
+      fontFamily: fontFamily,
+      fontSize: 30
+  );
+}
+
+TextStyle myTextStyle40({
+  Color textColor=Colors.black,
+  String fontFamily= "mainFont",
+  FontWeight fontWeight= FontWeight.bold
+}){
+  return TextStyle(
+      color: textColor,
+      fontFamily: fontFamily,
+      fontSize: 40
+  );
+}
